25aff6db8bd4662dd044927a005083f59df8dbed
bintang.hari <bintang.hari@ui.ac.id>
2024-05-13 18:09:17 +0700

    init rest-assured-starter (include my work)

---
+++ rest-assured-bdd-starter-main/src/test/java/starter/utils/JsonSchema.java
@@ -0,0 +1,7 @@
+package starter.utils;
+
+public class JsonSchema {
+    public static final String ERROR_SCHEMA = "features/schema/error_schema.json";
+    public static final String GET_ALL_PRODUCTS_SCHEMA = "features/schema/Products/get_all_products_schema.json";
+    public static final String CREATE_NEW_PRODUCT_SCHEMA = "features/schema/Products/create_new_product.json";
+}
