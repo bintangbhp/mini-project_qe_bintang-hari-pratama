b201a38dee488fa00de37ab9055f10a9033bc602
bintang.hari <bintang.hari@ui.ac.id>
2024-05-13 21:27:25 +0700

    add productCategories folder

--- rest-assured-bdd-starter-main/src/test/java/starter/utils/JsonSchema.java
+++ rest-assured-bdd-starter-main/src/test/java/starter/utils/JsonSchema.java
@@ -2,7 +2,10 @@ package starter.utils;
 
 public class JsonSchema {
     public static final String ERROR_SCHEMA = "features/schema/error_schema.json";
+
     public static final String GET_ALL_PRODUCTS_SCHEMA = "features/schema/Products/get_all_products_schema.json";
     public static final String CREATE_NEW_PRODUCT_SCHEMA = "features/schema/Products/create_new_product.json";
     public static final String DELETE_PRODUCT_SCHEMA = "features/schema/Products/delete_product_schema.json";
+
+    public static final String CATEGORY_SCHEMA = "features/schema/ProductCategories/category_schema.json";
 }
