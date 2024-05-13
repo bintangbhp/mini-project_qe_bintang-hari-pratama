76624cc072151345290aabe1ad143e32d5311e93
bintang.hari <bintang.hari@ui.ac.id>
2024-05-13 19:15:49 +0700

    delete product

--- rest-assured-bdd-starter-main/src/test/java/starter/utils/JsonSchema.java
+++ rest-assured-bdd-starter-main/src/test/java/starter/utils/JsonSchema.java
@@ -4,4 +4,5 @@ public class JsonSchema {
     public static final String ERROR_SCHEMA = "features/schema/error_schema.json";
     public static final String GET_ALL_PRODUCTS_SCHEMA = "features/schema/Products/get_all_products_schema.json";
     public static final String CREATE_NEW_PRODUCT_SCHEMA = "features/schema/Products/create_new_product.json";
+    public static final String DELETE_PRODUCT_SCHEMA = "features/schema/Products/delete_product_schema.json";
 }
